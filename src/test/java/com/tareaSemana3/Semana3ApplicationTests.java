e16d64e3c191c930b97279dfacfc00f61493d5ad
Pablo <User@Pablo>
2022-10-03 18:44:45 -0600

    Commit clase 3

---
+++ src/test/java/com/tareaSemana3/Semana3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tareaSemana3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
